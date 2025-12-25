d852ca4518aaf0e454868bc659d4505ee4f89e94
Noah Day <sheltielover432@gmail.com>
2025-12-24 22:33:06 -0500

    Basic Input and Uniforms

--- source/main.cpp
+++ source/main.cpp
@@ -4,14 +4,40 @@
 #include <vector>
 #include <string>
 
-int main()
-{
+struct Vec2 {
+    float x = 0.0f;
+    float y = 0.0f;
+};
+
+Vec2 offset;
+
+void keyCallback(GLFWwindow *window, int key, int scanCode, int action, int mods) {
+    if (action == GLFW_PRESS) {
+        switch (key) {
+            case GLFW_KEY_UP:
+                offset.y += 0.01f;
+                break;
+            case GLFW_KEY_DOWN:
+                offset.y -= 0.01f;
+                break;
+            case GLFW_KEY_RIGHT:
+                offset.x += 0.01f;
+                break;
+            case GLFW_KEY_LEFT:
+                offset.x -= 0.01f;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+int main() {
 #if defined (__linux__)
     glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
 #endif
     // Initialize GLFW
-    if (!glfwInit())
-    {
+    if (!glfwInit()) {
         return -1;
     }
 
@@ -19,19 +45,19 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "GameDevelopmentProject", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "GameDevelopmentProject", nullptr, nullptr);
 
-    if (window == nullptr)
-    {
+    if (window == nullptr) {
         std::cout << "Error creating window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+    glfwSetKeyCallback(window, keyCallback);
+
     glfwMakeContextCurrent(window);
 
-    if (glewInit() != GLEW_OK)
-    {
+    if (glewInit() != GLEW_OK) {
         glfwTerminate();
         return -1;
     }
@@ -41,24 +67,25 @@ int main()
         layout (location = 0) in vec3 position;
         layout (location = 1) in vec3 color;
 
+        uniform vec2 uOffset;
+
         out vec3 vColor;
 
         void main()
         {
             vColor = color;
-            gl_Position = vec4(position.x, position.y, position.z, 1.0);
+            gl_Position = vec4(position.x + uOffset.x, position.y + uOffset.y, position.z, 1.0);
         }
     )";
 
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    const char* vertexShaderCStr = vertexShaderSource.c_str();
+    const char *vertexShaderCStr = vertexShaderSource.c_str();
     glShaderSource(vertexShader, 1, &vertexShaderCStr, nullptr);
     glCompileShader(vertexShader);
 
     GLint success;
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
+    if (!success) {
         char infoLog[512];
         glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
         std::cerr << "ERROR:VERTEX_SHADER_COMPILATION_FAILED: " << infoLog << std::endl;
@@ -69,21 +96,21 @@ int main()
         out vec4 FragColor;
 
         in vec3 vColor;
+        uniform vec4 uColor;
 
         void main()
         {
-            FragColor = vec4(vColor, 1.0);
+            FragColor = vec4(vColor, 1.0) * uColor;
         }
     )";
 
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    const char* fragmentShaderSourceCStr = fragmentShaderSource.c_str();
+    const char *fragmentShaderSourceCStr = fragmentShaderSource.c_str();
     glShaderSource(fragmentShader, 1, &fragmentShaderSourceCStr, nullptr);
     glCompileShader(fragmentShader);
 
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
+    if (!success) {
         char infoLog[512];
         glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
         std::cerr << "ERROR:FRAGMENT_SHADER_COMPILATION_FAILED: " << infoLog << std::endl;
@@ -95,8 +122,7 @@ int main()
     glLinkProgram(shaderProgram);
 
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
-    {
+    if (!success) {
         char infoLog[512];
         glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
         std::cerr << "ERROR:SHADER_PROGRAM_LINKING_FAILED: " << infoLog << std::endl;
@@ -107,9 +133,9 @@ int main()
 
     std::vector<float> vertices =
     {
-        0.0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-        0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
         0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f
     };
 
@@ -137,21 +163,25 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * sizeof(float), (void *) (3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-    while (!glfwWindowShouldClose(window))
-    {
+    GLint uColorLoc = glGetUniformLocation(shaderProgram, "uColor");
+    GLint uOffsetLoc = glGetUniformLocation(shaderProgram, "uOffset");
+
+    while (!glfwWindowShouldClose(window)) {
         glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
+        glUniform4f(uColorLoc, 0.0f, 1.0f, 0.0f, 1.0f);
+        glUniform2f(uOffsetLoc, offset.x, offset.y);
         glBindVertexArray(vao);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
