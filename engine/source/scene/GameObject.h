f128cc02c6c9a78a18913a2bad169823cac306bf
Noah Day <sheltielover432@gmail.com>
2026-01-13 21:49:12 -0500

    feat: Gameobjects and Scene

---
+++ engine/source/scene/GameObject.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace eng {
+    class GameObject {
+    public:
+        virtual ~GameObject() = default;
+
+        virtual void Update(float deltaTime);
+
+        const std::string &GetName() const;
+
+        void SetName(const std::string &name);
+
+        GameObject *GetParent();
+
+        bool IsAlive() const;
+
+        void MarkForDestroy();
+
+    private:
+        GameObject() = default;
+
+        std::string m_name;
+        GameObject *m_parent = nullptr;
+        std::vector<std::unique_ptr<GameObject> > m_children;
+        bool m_isAlive = true;
+
+        friend class Scene;
+    };
+}
